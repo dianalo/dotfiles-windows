a99a1bc43f582fdf339f609b97a882b6d3042d82
Loris Diana <8928034+dianalo@users.noreply.github.com>
2020-10-07 17:41:34 +0200

    update

--- exports.ps1
+++ exports.ps1
@@ -1,5 +1,5 @@
 # Make vim the default editor
-Set-Environment "EDITOR" "code --wait"
+Set-Environment "EDITOR" "code-insiders --wait"
 Set-Environment "GIT_EDITOR" $Env:EDITOR
 
 # Disable the Progress Bar
--- windows.ps1
+++ windows.ps1
@@ -213,7 +213,7 @@ powercfg /change /standby-timeout-ac 1440
 Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" "EnableSuperfetch" 0
 
 # Network: Disable WiFi Sense
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config" "AutoConnectAllowedOEM" 0
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config" "AutoConnectAllowedOEM" 1
 
 ###############################################################################
 ### Explorer, Taskbar, and System Tray                                        #
